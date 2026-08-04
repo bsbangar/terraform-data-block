eeda90744d79e1b431cb66250063fd0674831c01
Balwinder Singh <itssinghb@gmail.com>
2026-08-04 14:09:22 +0100

    update lb1

--- Child_module/azurerm_lb/data.tf
+++ Child_module/azurerm_lb/data.tf
@@ -3,3 +3,22 @@ data "azurerm_public_ip" "pip_data" {
   name = each.value.name-pip
   resource_group_name = each.value.resource_group_name
 }
+
+data "azurerm_lb" "lbsc" {
+  for_each = var.lbsc
+  name                = each.value.name
+  resource_group_name = each.value.resource_group_name
+}
+
+
+data "azurerm_virtual_network" "lbsc" {
+  for_each = var.lbsc
+  name                = each.value.name-vnet
+  resource_group_name = each.value.resource_group_name
+}
+
+data "azurerm_lb_backend_address_pool" "lbsc" {
+  for_each = var.lbsc
+  name            = each.value.name-pool
+  loadbalancer_id = data.azurerm_lb.lbsc[each.key].id
+}
--- Child_module/azurerm_lb/main.tf
+++ Child_module/azurerm_lb/main.tf
@@ -4,8 +4,23 @@ resource "azurerm_lb" "lbsc" {
   location            = each.value.location
   resource_group_name = each.value.resource_group_name
 
-#   frontend_ip_configuration {
-#     name                 = "PublicIPAddress"
-#     public_ip_address_id = azurerm_public_ip.example.id
-#   }
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = data.azurerm_public_ip.pip_data[each.key].id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "lbsc-pool" {
+  for_each = var.lbsc
+  loadbalancer_id = data.azurerm_lb.lbsc[each.key].id
+  name            = each.value.name-pool
+  virtual_network_id = data.azurerm_virtual_network.lbsc[each.key].id
+}
+
+resource "azurerm_lb_backend_address_pool_address" "lbsc-pool-address" {
+  for_each = var.lbsc
+  name                    = each.value.name-pool-address
+  backend_address_pool_id = data.azurerm_lb_backend_address_pool.lbsc[each.key].id
+  virtual_network_id      = data.azurerm_virtual_network.lbsc[each.key].id
+  ip_address              = "10.1.1.4"
 }
